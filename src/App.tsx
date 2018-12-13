6b5f8784c0285b4154afd1a2380ae9b445abb8d9
sisisin <azsisisin@gmail.com>
2018-12-14 00:01:58 +0900

    feat: setup ui-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
+import { UIRouter, UIView, UISref } from '@uirouter/react';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { routerPlugins, routerStates } from './router.config';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <UIRouter plugins={routerPlugins} states={routerStates}>
+        <>
+          <ul>
+            <li>
+              <UISref to="home">
+                <a>home</a>
+              </UISref>
+            </li>
+            <li>
+              <UISref to="user">
+                <a>user</a>
+              </UISref>
+            </li>
+          </ul>
+
+          <UIView />
+        </>
+      </UIRouter>
     );
   }
 }
